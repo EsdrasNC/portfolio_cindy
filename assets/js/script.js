45e06b0cf7dcb8c2ae3aefd1548687597794dde4
Esdras Neres Carneiro <esdras.nc@icloud.com>
2024-12-26 23:55:52 -0300

    atualização css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.querySelectorAll(".nav_bar a").forEach((link) => {
     e.preventDefault();
     const targetId = this.getAttribute("href").substring(1); // Pega o ID do link
     const targetElement = document.getElementById(targetId);
-    const offset = 200; // Altura do espaço que você quer deixar
+    const offset = 150; // Altura do espaço que você quer deixar
     const topPosition = targetElement.offsetTop - offset;
 
     window.scrollTo({
